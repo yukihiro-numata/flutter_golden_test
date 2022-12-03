c66c0e389581d85674360b2acc4f83531d737fee
yukihiro-numata <yuki.numata97@gmail.com>
2022-12-03 14:29:09 +0900

    Update my app golden test from size to device builder.

--- test/my_app_golden_test.dart
+++ test/my_app_golden_test.dart
@@ -1,16 +1,25 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_golden_test/main.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
 
 void main() {
   const targetWidget = MyApp();
-  const targetSize = Size(392, 813);
 
   testGoldens(
     'my_app',
     (WidgetTester tester) async {
-      await tester.pumpWidgetBuilder(targetWidget, surfaceSize: targetSize);
+      final builder = DeviceBuilder()
+        ..overrideDevicesForAllScenarios(
+          devices: [
+            Device.phone,
+            Device.iphone11,
+          ],
+        )
+        ..addScenario(
+          widget: targetWidget,
+          name: 'my_app',
+        );
+      await tester.pumpDeviceBuilder(builder);
       await screenMatchesGolden(tester, 'my_app');
     },
   );
